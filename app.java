fec7543105f1e42f2b53219b66c634a320e4fde0
abhishekalluri <abhishekvarmaalluri30@gmail.com>
2026-04-13 19:38:06 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,3 +1,6 @@
-adding java code
-not changed 
-mmmmmmmmmmmmm
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
